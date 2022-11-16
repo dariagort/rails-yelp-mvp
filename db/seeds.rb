dbc89f861ed971f9ea87371f69690a1ae2a6d691
Daria Gortchacow <daria.gortchacow@gmail.com>
2022-11-16 16:49:57 +0100

    adding restaurants and reviews

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+5.times do
+  category = %w(chinese italian japanese french belgian)
+  name = Faker::TvShows::TwinPeaks.character
+  address = Faker::TvShows::TwinPeaks.location
+  @restaurant = Restaurant.new(name: name, address: address, category: category.sample)
+  @restaurant.save
+end
